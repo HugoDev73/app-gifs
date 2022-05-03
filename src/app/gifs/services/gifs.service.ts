6b5830caa6d02cf93b75ad6b67d6c1f7aee3ce4b
Hugo Mendoza <hugo.dev73@gmail.com>
2022-05-03 13:20:17 -0500

    first commit

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchGIFResponse } from '../interfaces/gifs.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GifsService {
+
+  private apiKey: string = 'iYFHd5VKJxbwz8mkQvsLxk6Byzv9eMFw';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
+  private _historial: string[] = [];
+
+  public resultados:Gif[] = [];
+
+  constructor(private http: HttpClient) {
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+  }
+
+  get historial() {
+    return [...this._historial];
+  }
+
+  buscarGifs(query: string) {
+    query = query.trim().toLocaleLowerCase();
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0, 10);
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+    }
+
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', query);
+
+   /*  const url: string =
+      `/search?api_key=iYFHd5VKJxbwz8mkQvsLxk6Byzv9eMFw&q=${query}&limit=10`;
+ */
+
+    this.http.get<SearchGIFResponse>(`${this.servicioUrl}/search`, {params}).subscribe((resp: any) => {
+      this.resultados = resp.data;
+      localStorage.setItem('resultados', JSON.stringify(this.resultados));
+    });
+
+  }
+}
